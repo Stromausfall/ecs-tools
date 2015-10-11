b69e4772a88f85b45f820046e14f47769b2ed996
Matthias Auer <mail@matthias-auer.net>
2015-10-11 11:15:56 +0200

    the user now has to provide the atlas files that should be used for the TextureLoader

--- src/net/matthiasauer/ecstools/graphics/texture/TextureLoader.java
+++ src/net/matthiasauer/ecstools/graphics/texture/TextureLoader.java
@@ -1,33 +1,61 @@
 package net.matthiasauer.ecstools.graphics.texture;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 
 public final class TextureLoader {
-	private TextureLoader() {
-		this.archive = new HashMap<String, AtlasRegion>();
-		this.textureAtlas =
-				new TextureAtlas(
-						Gdx.files.internal(TEXTURE_ATLAS_FILE));
+	private List<TextureAtlas> create(List<String> atlasFilePaths) {
+		List<TextureAtlas> result = new LinkedList<TextureAtlas>();
+		
+		for (String atlasFilePath : atlasFilePaths) {
+			FileHandle atlasFile = Gdx.files.internal(atlasFilePath);
+			TextureAtlas textureAtlas = new TextureAtlas(atlasFile);
+			result.add(textureAtlas);
+		}
+		
+		return result;
 	}
 	
-	private static final String TEXTURE_ATLAS_FILE = "data1.atlas";
+	private TextureLoader(List<String> atlasFilePaths) {
+		this.archive = new HashMap<String, AtlasRegion>();
+		this.textureAtlases =
+				Collections.unmodifiableList(create(atlasFilePaths));
+	}
+	
 	private static TextureLoader instance = null;
 	private final Map<String, AtlasRegion> archive;
-	private final TextureAtlas textureAtlas;
+	private final List<TextureAtlas> textureAtlases;
+	
+	public static void createInstance(List<String> atlasFilePaths) {
+		if (instance != null) {
+			throw new NullPointerException("the create method may only be called once !");
+		}
+		
+		instance = new TextureLoader(atlasFilePaths);
+	}
 	
 	public static TextureLoader getInstance() {
 		if (instance == null) {
-			instance = new TextureLoader();
+			throw new NullPointerException("the TextureLoader.createInstance method was not called before requesting the instance !!!");
 		}
 		
 		return instance;
 	}
 	
+	/**
+	 * returns the AtlasRegion with the given name,
+	 * Note : it is expected that the name is unique across ALL used TextureAtlases
+	 * @param name
+	 * @return
+	 */
 	public AtlasRegion getTexture(String name) {
 		AtlasRegion texture = null;
 		
@@ -37,10 +65,13 @@ public final class TextureLoader {
 		
 		// if there is no entry - load it and save it
 		if (!archive.containsKey(name)) {
-			texture = textureAtlas.findRegion(name);
-			
-			if (texture != null) {
-				archive.put(name, texture);
+			for (TextureAtlas textureAtlas : this.textureAtlases) {
+				texture = textureAtlas.findRegion(name);
+				
+				if (texture != null) {
+					archive.put(name, texture);
+					break;
+				}
 			}
 		}
 		
@@ -48,7 +79,7 @@ public final class TextureLoader {
 		
 		if (texture == null) {
 			throw new NullPointerException(
-					"found no region '" + name + "' in TextureAtlas '" + TEXTURE_ATLAS_FILE + "'");
+					"found no region '" + name + "' in the TextureAtlases");
 		}
 		
 		return texture;
